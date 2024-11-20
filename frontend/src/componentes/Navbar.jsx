4d32c4340656e7b8f8c128157033ad6fbb73b257
Ana <anabqs0107@gmail.com>
2024-11-20 02:03:01 -0600

    Modal registro clientes listo

--- frontend/src/componentes/Navbar.jsx
+++ frontend/src/componentes/Navbar.jsx
@@ -1,25 +1,60 @@
-import React from 'react';
-import { Button } from 'primereact/button';
-import "primereact/resources/themes/lara-light-cyan/theme.css";  
-import "primereact/resources/primereact.min.css";            
-import "primeicons/primeicons.css";  
-
+import React, { useState } from "react";
+import { Button } from "primereact/button";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import RegistrarCliente from "../Modals/RegistrarCliente";
 
 const Navbar = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleRegister = (formData) => {
+    console.log("Datos registrados:", formData);
+    handleCloseModal();
+  };
+
   return (
-    <div className="login-wrapper">
-      <div className="nav">
-        <div className="logo">
-        <img src="/img/navbar.png"  alt="Logo" style={{ width: '40px', height: '40px' }} />
+    <>
+      <div className="login-wrapper">
+        <div className="nav">
+          <div className="logo">
+            <img
+              src="/img/navbar.png"
+              alt="Logo"
+              style={{ width: "40px", height: "40px" }}
+            />
+          </div>
+          <div className="nav-links">
+            <a href="#" className="nav-link">
+              Inicio
+            </a>
+            <a href="#" className="nav-link">
+              Opciones de Préstamo
+            </a>
+            <a href="#" className="nav-link">
+              Contáctenos
+            </a>
+          </div>
+          <RegistrarCliente
+        visible={isModalVisible}
+        onHide={handleCloseModal} // Cierra el modal
+        onRegister={handleRegister} // Maneja el registro
+            className="join-btn"
+      />
         </div>
-        <div className="nav-links">
-          <a href="#" className="nav-link">Inicio</a>
-          <a href="#" className="nav-link">Opciones de Préstamo</a>
-          <a href="#" className="nav-link">Contáctenos</a>
-        </div>
-        <Button label="Registrarse" className="join-btn" />
       </div>
-    </div>
+
+      {/* Modal asociado al botón "Registrarse" */}
+     
+    </>
   );
 };
 

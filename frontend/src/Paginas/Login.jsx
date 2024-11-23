9c4692adb19e99cc34bd7e7d339c91dddf04f46b
Ana <anabqs0107@gmail.com>
2024-11-23 00:26:22 -0600

    Actualización de front

--- frontend/src/Paginas/Login.jsx
+++ frontend/src/Paginas/Login.jsx
@@ -9,39 +9,42 @@ const Login = () => {
     const [password, setPassword] = useState('');
 
     return (
-            <div className="login-container">
-                <div className="login-section">
-                    <div className="login-header">
-                        <span className="active">Inicio de Sesión</span>
-                    </div>
-                    
-                    <div className="login-form">
-                        <span className="p-input-icon-left">
-                            <i className="pi pi-user" />
-                            <InputText
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                placeholder="Digite número de cédula"
-                            />
-                        </span>
-
-                        <span className="p-input-icon-left">
-                            <i className="pi pi-lock" />
-                            <InputText
-                                type="password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                placeholder="Contraseña"
-                            />
-                        </span>
-
-                        <Button label="Iniciar Sesión" className="login-btn" />
-                    </div>
+        <div className="login-container">
+            <div className="login-section">
+                <div className="login-header">
+                    <span className="active">Inicio de Sesión</span>
                 </div>
-                <div className="image-section">
-                    <img src="/img/login.png" alt="Decorative" className='imagenLogin' />
+
+                <div className="login-form">
+                    <span className="p-input-icon-left">
+                        <i className="pi pi-user" />
+                        <InputText
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder="Digite número de cédula"
+                        />
+                    </span>
+
+                    <span className="p-input-icon-left">
+                        <i className="pi pi-lock" />
+                        <InputText
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="Contraseña"
+                        />
+                    </span>
+
+                    <Button label="Iniciar Sesión"  className="login-btn" />
                 </div>
             </div>
+            <div className="image-section">
+                <div class="circle"></div>
+                <div class="circle"></div>
+                <div class="circle"></div>
+                <img src="/img/login.png" alt="Decorative" className='imagenLogin' />
+            </div>
+        </div>
     );
 };
 
